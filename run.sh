b77873907404ef39b1f00b0bd5f7706041aa3aee
xxll222557 <116071096+xxll222557@users.noreply.github.com>
2024-05-01 15:48:52 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 sudo apt update
-curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -
-sudo apt install nodejs
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt-get install -y nodejs
 sudo npm install cross-env
 sudo npm install -D vuepress
 sudo npm run docs:serve
